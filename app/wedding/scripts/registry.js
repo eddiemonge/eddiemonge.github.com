ffc3ffb4f4cbac156772000ce1f8fcb94804a536
Eddie Monge <eddie@eddiemonge.com>
2015-04-26 14:18:14 -0700

    update honeymoon site

---
+++ app/wedding/scripts/registry.js
@@ -0,0 +1,80 @@
+var items = [
+  {
+    name: 'Flight',
+    caption: 'Flight to Florida',
+    description: 'We are flying to Florida the day after the wedding and staying for a week.',
+    image: 'airplane',
+    price: 100,
+    quantity: 8
+  }, {
+    name: 'Cruise',
+    caption: 'Cruise to the Bahamas',
+    description: 'We are taking a 4 day cruise to the Bahamas out of Miami.',
+    image: 'cruise',
+    price: 125,
+    quantity: 14
+  }, {
+    name: 'DisneyWorld',
+    caption: 'DisneyWorld',
+    description: 'A 2 day visit to DisneyWorld.',
+    image: 'disneyworld',
+    price: 40,
+    quantity: 8
+  }, {
+    name: 'Snorkeling',
+    caption: 'Sea Safari Snorkel By Boat',
+    description: 'Board a spacious catamaran and sail to a shallow coral reef where you will snorkel in the clear waters amidst colorful fish and brilliant coral. Take a plunge into the sparkling sea from the vessel\'s waterslide or scale the 30 foot rock climbing wall.',
+    image: 'snorkeling',
+    price: 25,
+    quantity: 5
+  }, {
+    name: 'Kayaking',
+    caption: 'Kayak Tour',
+    description: 'The beautiful turquoise waters south of Great Stirrup Cay are perfect for a leisurely guided kayak tour. You\'ll paddle over the shallow clear waters where you may encounter a variety of marine life.',
+    image: 'kayak',
+    price: 20,
+    quantity: 5
+  }, {
+    name: 'BeachSnorkel',
+    caption: 'Deserted Island Beach & Snorkel',
+    description: 'Embark on an eco-adventure tour to a deserted beach aboard a 45-foot motorized catamaran. You\'ll snorkel over an open shallow coral reef, home to hundreds of colorful fish.',
+    image: 'shore',
+    price: 25,
+    quantity: 8
+  }, {
+    name: 'Atv',
+    caption: 'Off Road ATV & Beach',
+    description: 'After gearing up and attending a safety briefing, you\'re now ready to explore the winding trails through dense native vegetation. You\'ll make several stops along the way and after, you\'ll visit the Junkanoo Beach Club',
+    image: 'atv',
+    price: 40,
+    quantity: 6
+  }, {
+    name: 'PaddleBoard',
+    caption: 'Stand Up Paddle Board',
+    description: 'Stand up Paddle Boarding or SUP, is a fun sport and very easy to learn. With just a few helpful hints from our staff, you\'ll be able to paddle Bertram\'s Cove offering views of the beaches and glimpses of marine life.',
+    image: 'paddleboard',
+    price: 20,
+    quantity: 2
+  }, {
+    name: 'WaveRunner',
+    caption: 'Wave Runner Tour',
+    description: 'Take a 45-minute guided adventure through the Berry Islands, neighboring Great Stirrup Cay. After a safety briefing and orientation, you\'ll board your Wave Runner for an informative and exciting guided tour',
+    image: 'waverunner',
+    price: 30,
+    quantity: 5
+  }, {
+    name: 'BeachDay',
+    caption: 'Blue Lagoon Island Beach Day',
+    description: 'Start your day with a scenic boat ride through historic Nassau Harbour towards Blue Lagoon Island. Enjoy this private island at your leisure. Swim on a pristine beach or just relax and lounge in a double hammock.',
+    image: 'beach',
+    price: 20,
+    quantity: 6
+  }, {
+    name: 'Parasail',
+    caption: 'Parasail',
+    description: 'Sail 400 feet above the blue waters of the Bahamas in tandem while being towed by a brand-new parasail boat, just off the beach of Great Stirrup Cay.',
+    image: 'parasail',
+    price: 40,
+    quantity: 4
+  }
+];
